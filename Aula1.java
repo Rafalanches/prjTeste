9eb18f4756d8a141da7d3ef7de6b9036873a6ad5
Rafalanches <133991142+Rafalanches@users.noreply.github.com>
2023-06-06 12:30:53 -0300

    Add files via upload

---
+++ Aula1.java
@@ -0,0 +1,15 @@
+package prjTeste;
+
+import java.util.Scanner;
+
+public class Aula1 {
+	
+	public static void main(String[] args) {
+	String nome;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Digite seu nome meu nobre: ");
+	nome = sc.nextLine();
+	System.out.println("Meu nobre, seu nome é "+nome);
+	}
+
+}
---
+++ Aula2.java
@@ -0,0 +1,11 @@
+package prjTeste;
+
+public class Aula2 {
+	
+	int a, b=55, c=83;
+	a = b+c;
+	
+	System.out.println("Meu nobre, seu número é: "+a);
+	
+	
+}
